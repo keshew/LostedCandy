86c2a05905e1e852fa825e47b55c555aca1b2d45
keshew <artyom.korotckov2017@yandex.ru>
2024-12-12 14:13:49 +0500

    Initial Commit

---
+++ LostedCandy/LostedCandyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LostedCandyApp.swift
+//  LostedCandy
+//
+//  Created by Артём Коротков on 12.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct LostedCandyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
